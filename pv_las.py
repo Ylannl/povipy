49b354894edd4adfd27a612213fb6e9225f10855
Ylannl <r.y.peters@tudelft.nl>
2016-02-01 15:02:11 +0100

    add thinning option to a target point count in las viewer

--- pv_las.py
+++ pv_las.py
@@ -4,6 +4,7 @@ from povi import App
 import numpy as np
 
 from laspy.file import File
+from laspy.util import LaspyException
 
 INFILE = 'data/ahn3_zoetermeer_stadshart_small.laz'
 
@@ -12,70 +13,88 @@ if __name__ == '__main__':
     
     if len(sys.argv) > 1:
         INFILE = sys.argv[1]
+    TARGET_PT_COUNT = 1.0E6
 
-    t1=time()
+    t0=time()
     lasfile = File(INFILE)
-    
     mi, ma = np.array(lasfile.header.min), np.array(lasfile.header.max)    
-    offset = mi + (ma - mi)
-    # import ipdb; ipdb.set_trace()
     count = lasfile.header.count
+    offset = mi + (ma - mi)
+    print "{} points loaded from file in {} s".format(count, time()-t0)
+    # import ipdb; ipdb.set_trace()
+
+    filt = np.random.random(count) < (TARGET_PT_COUNT / count)
+    thin_count = np.sum(filt)
+    print "{} points remaining after thinning [factor: {:.2f}]".format(thin_count, float(thin_count)/count)
+
     points = np.stack((lasfile.get_x_scaled(), lasfile.get_y_scaled(), lasfile.get_z_scaled()), axis=1) - offset
-    classification = lasfile.get_classification()
-    
-    intensity = lasfile.get_intensity().astype(np.float32)
-    intensity = np.clip(intensity, 0, 600)
-
-    gps_time = lasfile.get_gps_time().astype(np.float32)
-    gps_time[np.argsort(gps_time)] = np.arange(count)
-
-    scan_angle_rank = lasfile.get_scan_angle_rank().astype(np.float32)
-    return_num = lasfile.get_return_num().astype(np.float32)
-    
-    # filt = np.logical_and(ma.ma_radii < 190., ma.ma_segment>0)
-    # filt = ma.ma_segment>=0
-    # filt = ma.ma_segment>0
-    t2=time()
-    print "loaded {} points in {} s".format(count, t2-t1)
-    
     c.add_data_source(
         opts=['splat_point'],
-        points=points,
+        points=points[filt],
         colormap='terrain'
     )
+    c.data_programs[0].toggle_visibility()
 
-    c.add_data_source(
-        opts=['splat_point', 'with_intensity'],
-        points=points,
-        category=classification.astype(np.float32),
-        colormap='las_classes'
-    )
+    try:
+        classification = lasfile.get_classification()
+        if classification.max() == 0: raise LaspyException
+        c.add_data_source(
+            opts=['splat_point', 'with_intensity'],
+            points=points[filt],
+            category=classification.astype(np.float32)[filt],
+            colormap='las_classes'
+        )
+    except LaspyException:
+        pass
+    
+    try:
+        return_num = lasfile.get_return_num().astype(np.float32)
+        if return_num.max() == 1: raise LaspyException
+        c.add_data_source(
+            opts=['splat_point', 'with_intensity'],
+            points=points[filt],
+            intensity=return_num[filt],
+            colormap='jet'
+        )
+    except LaspyException:
+        pass
 
-    c.add_data_source(
-        opts=['splat_point', 'with_intensity'],
-        points=points,
-        intensity=intensity,
-        colormap='jet'
-    )
+    try:
+        intensity = lasfile.get_intensity().astype(np.float32)
+        intensity = np.clip(intensity, 0, 600)
+        if intensity.max() == 0: raise LaspyException
+        c.add_data_source(
+            opts=['splat_point', 'with_intensity'],
+            points=points[filt],
+            intensity=intensity[filt],
+            colormap='jet'
+        )
+    except LaspyException:
+        pass
 
-    c.add_data_source(
-        opts=['splat_point', 'with_intensity'],
-        points=points,
-        intensity=gps_time,
-        colormap='jet'
-    )
-
-    c.add_data_source(
-        opts=['splat_point', 'with_intensity'],
-        points=points,
-        intensity=scan_angle_rank,
-        colormap='jet'
-    )
-    c.add_data_source(
-        opts=['splat_point', 'with_intensity'],
-        points=points,
-        intensity=return_num,
-        colormap='jet'
-    )
+    try:
+        scan_angle_rank = lasfile.get_scan_angle_rank().astype(np.float32)
+        if scan_angle_rank.max() == 0: raise LaspyException
+        c.add_data_source(
+            opts=['splat_point', 'with_intensity'],
+            points=points[filt],
+            intensity=scan_angle_rank[filt],
+            colormap='jet'
+        )
+    except LaspyException:
+        pass
+    
+    try:
+        gps_time = lasfile.get_gps_time().astype(np.float32)
+        if gps_time.max() == 0: raise LaspyException
+        gps_time[np.argsort(gps_time)] = np.arange(count)
+        c.add_data_source(
+            opts=['splat_point', 'with_intensity'],
+            points=points[filt],
+            intensity=gps_time[filt],
+            colormap='jet'
+        )
+    except LaspyException:
+        pass
 
     c.run()
