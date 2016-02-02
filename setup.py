d01c23e8c066062942213bc144b79bd577fa2b96
Ylannl <r.y.peters@tudelft.nl>
2016-02-02 14:38:32 +0100

    trying to fix pointio installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version = "0.1",
     packages = find_packages(),
     install_requires = ["numpy>=1.9.1", "pyopengl", "glfw", "click", "pointio", "laspy"],
-    dependency_links = ["git+https://github.com/Ylannl/pointio.git#egg=pointio"],
+    dependency_links = ["https://github.com/Ylannl/pointio.git/tarbal/master#egg=pointio-0.1"],
     # extras_require = {
     #     'LAS':  ["laspy"],
     #     'numba': ["numba"],
