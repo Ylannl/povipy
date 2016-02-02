cff9c027ec5643463197d8d1dd77d9a8f59d5184
Ylannl <r.y.peters@tudelft.nl>
2016-02-02 16:19:37 +0100

    fix pointio link

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ setup(
     name = "povi",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ["numpy>=1.9.1", "pyopengl", "glfw", "click", "pointio", "laspy"],
-    dependency_links = ["https://github.com/Ylannl/pointio.git/tarbal/master#egg=pointio-0.1"],
+    install_requires = ["numpy>=1.9.1", "pyopengl", "glfw", "click", "pointio==0.1", "laspy"],
+    dependency_links = ["http://github.com/Ylannl/pointio/tarball/master#egg=pointio-0.1"],
     # extras_require = {
     #     'LAS':  ["laspy"],
     #     'numba': ["numba"],
